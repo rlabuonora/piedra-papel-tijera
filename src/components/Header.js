a34aea63e3743bb884b04adf8a1b590f1360ced3
rafa <rlabuonora@yahoo.com>
2021-12-28 15:11:47 -0300

    Logo as img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from '../css/images/logo.svg';
 
 const Header = (props) => {
     return(
         <div className="head">
+          <img src={logo} alt="Game Logo" />
           <div className="score"> Score
             <span>{ props.score }</span>
           </div>
